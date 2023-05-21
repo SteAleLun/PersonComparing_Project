9cb02af7bd39d6e6a4b8ccf14e0b4e71e1679fe2
SteAleLun <stealelun@gmail.com>
2023-05-21 04:39:00 +0400

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
 
         Person p1 = new Person("Ignacio", "Varga", 34);
         Person p2 = new Person("Hector", "Salamanca", 70);
-        Person p3= new Person("Don", "Eladio Vuente", 50);
-        Person p4= new Person("Pedro", "Sanches Luego Ciano", 42);
-        Person p5= new Person("Hulio", "Alvares Santa Muerta", 29);
+        Person p3 = new Person("Don", "Eladio Vuente", 50);
+        Person p4 = new Person("Pedro", "Sanches Luego Ciano", 42);
+        Person p5 = new Person("Hulio", "Alvares Santa Muerta", 29);
 
         ArrayList<Person> persons = new ArrayList<>();
 
@@ -17,10 +18,21 @@ public class Main {
         persons.add(p1);
         persons.add(p2);
 
-        persons.sort(new CompareByGentry().thenComparing(new CompareByAge()));
+
+        Comparator<Person> compareByGentry =
+                (o1, o2) ->  wordCount(o1.getSurName()) - wordCount(o2.getSurName()) ;
+
+        Comparator<Person> compareByAge =
+                (o1, o2) -> o1.getAge() - o2.getAge();
+
+        persons.sort(compareByGentry.thenComparing(compareByAge));
 
         System.out.println(persons.toString());
+    }
 
+    private static int wordCount(String string) {
+        String[] words = string.split(" ");
+        return words.length;
     }
 
 }
